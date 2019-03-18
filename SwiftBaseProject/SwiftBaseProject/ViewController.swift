6aaec61c76ea5a5c121d88eb27c93fd8d25c7206
王丹 <1653591454@qq.com>
2019-03-18 09:06:40 +0800

    1.创建一个新的Swift项目

---
+++ SwiftBaseProject/SwiftBaseProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftBaseProject
+//
+//  Created by sh-lx on 2019/3/18.
+//  Copyright © 2019年 WangDan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
