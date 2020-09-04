642fc86f52692dc2cd8e5618badb05bc9ffb65d8
Fan <shdjxywd@sina.com>
2020-09-04 12:08:40 +0800

    添加了一些分类extension

--- SwiftBaseProject/SwiftBaseProject/Class/CommonFile/Extension+Swift/String+extension.swift
+++ SwiftBaseProject/SwiftBaseProject/Class/CommonFile/Extension+Swift/String+extension.swift
@@ -128,3 +128,59 @@ extension String {
         
     }
 }
+
+extension String{
+    
+    /// 正则匹配手机号
+    var isMobile: Bool {
+        /**
+         * 手机号码
+         * 移动：134 135 136 137 138 139 147 148 150 151 152 157 158 159  165 172 178 182 183 184 187 188 198
+         * 联通：130 131 132 145 146 155 156 166 171 175 176 185 186
+         * 电信：133 149 153 173 174 177 180 181 189 199
+         * 虚拟：170
+         */
+        return isMatch("^(1[3-9])\\d{9}$")
+    }
+    
+    /// 正则匹配用户身份证号15或18位
+    var isUserIdCard: Bool {
+        return isMatch("(^[0-9]{15}$)|([0-9]{17}([0-9]|X)$)")
+    }
+    
+    /// 正则匹配用户密码6-12位数字和字母组合
+    var isPassword: Bool {
+        return isMatch("^(?![0-9]+$)(?![a-zA-Z]+$)[a-zA-Z0-9]{6,12}")
+    }
+    
+    /// 正则匹配URL
+    var isURL: Bool {
+        return isMatch("^[0-9A-Za-z]{1,50}")
+    }
+    
+    /// 正则匹配用户姓名,20位的中文或英文
+    var isUserName: Bool {
+        return isMatch("^[a-zA-Z\\u4E00-\\u9FA5]{1,20}")
+    }
+    
+    /// 正则匹配用户email
+    var isEmail: Bool {
+        return isMatch("[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}")
+    }
+    
+    /// 判断是否都是数字
+    var isNumber: Bool {
+        return isMatch("^[0-9]*$")
+    }
+    
+    /// 只能输入由26个英文字母组成的字符串
+    var isLetter: Bool {
+        return isMatch("^[A-Za-z]+$")
+    }
+    
+    private func isMatch(_ pred: String ) -> Bool {
+        let pred = NSPredicate(format: "SELF MATCHES %@", pred)
+        let isMatch: Bool = pred.evaluate(with: self)
+        return isMatch
+    }
+}
---
+++ SwiftBaseProject/SwiftBaseProject/Class/CommonFile/Extension+Swift/UIViewController+extentsion.swift
@@ -0,0 +1,53 @@
+//
+//  UIViewController+extentsion.swift
+//  SwiftBaseProject
+//
+//  Created by jujia on 2020/9/4.
+//  Copyright © 2020 WangDan. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+extension UIViewController {
+    // MARK: - 查找顶层控制器、
+    // 获取顶层控制器 根据window
+    func getTopVC() -> (UIViewController?) {
+        var window = UIApplication.shared.keyWindow
+        //是否为当前显示的window
+        if window?.windowLevel != UIWindow.Level.normal{
+            let windows = UIApplication.shared.windows
+            for  windowTemp in windows{
+                if windowTemp.windowLevel == UIWindow.Level.normal{
+                    window = windowTemp
+                    break
+                }
+            }
+        }
+        let vc = window?.rootViewController
+        return getTopVC(withCurrentVC: vc)
+    }
+    ///根据控制器获取 顶层控制器
+    func getTopVC(withCurrentVC VC :UIViewController?) -> UIViewController? {
+        if VC == nil {
+            print("🌶： 找不到顶层控制器")
+            return nil
+        }
+        if let presentVC = VC?.presentedViewController {
+            //modal出来的 控制器
+            return getTopVC(withCurrentVC: presentVC)
+        }else if let tabVC = VC as? UITabBarController {
+            // tabBar 的跟控制器
+            if let selectVC = tabVC.selectedViewController {
+                return getTopVC(withCurrentVC: selectVC)
+            }
+            return nil
+        } else if let naiVC = VC as? UINavigationController {
+            // 控制器是 nav
+            return getTopVC(withCurrentVC:naiVC.visibleViewController)
+        } else {
+            // 返回顶控制器
+            return VC
+        }
+    }
+}
